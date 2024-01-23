7c948d17daa94236cdaff693c52577c690410423
Carlos Rizo <JuanCarlos.RizoMendoza@mx.bosch.com>
2024-01-23 07:53:11 -0600

    Primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  // public onDelete: EventEmitter<int> = new EventEmitter();
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  // onDeleteCharacter(index: number):void {
+  //   this.onDelete.emit( index );
+  // }
+
+  deleteCharacterById( id?: string ):void {
+    if ( !id ) return;
+
+    // console.log( id );
+
+    this.onDelete.emit( id );
+  }
+
+}
